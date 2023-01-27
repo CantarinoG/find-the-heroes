9bb617a042ac55dd0ce9b6b116defde8ee68311b
CantarinoG <guilhermecantarino1709@gmail.com>
2023-01-27 16:04:06 -0300

    Style Header component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css";
 function Footer() {
 
     return <div className="footer">
-        Art by Todd Nauck. (Marvel Comics)
+        Art by Todd Nauck. (Marvel Comics) | All Characters Belong to Marvel Comics.
     </div>
 
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,15 @@ import "../styles/Header.css";
 function Header() {
 
     return <header>
-        <h1>Where Are The Heroes?</h1>
+        <h1>WHERE ARE THE HEROES?</h1>
         <div className="leaderboard">
-            <button>Leaderboard</button>
+            <button>SEE LEADERBOARD</button>
         </div>
         <div className="targets">
-            Left: A B C
+            LEFT: 
+            <div className="char" id="antman"></div>
+            <div className="char" id="daredevil"></div>
+            <div className="char" id="deadpool"></div>
         </div>
         <div className="timer">
             00:00
